0e9492908f1075d261d2efde878e40a794fbb213
JawadChaieb <jawad.chaieb@student.odisee.be>
2022-11-27 21:10:41 +0100

    1

--- github template(1)/SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
+++ github template(1)/SlnLes02VariabelenBerekeningen/ConsoleBmi/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class BMI
+{
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("BMI CALCULATOR");
+        Console.WriteLine("==============");
+
+        //Lengte tijpen
+        Console.Write("Lengte (in cm): ");
+        int lengteCm = Convert.ToInt32(Console.ReadLine());
+
+        //Gewicht tijpen
+        Console.Write("Gewicht (in kg):");
+        int gewicht = Convert.ToInt32(Console.ReadLine());
+
+        //omzetting cm naar m van Lengte
+        double lengteM = Convert.ToDouble(lengteCm) / 100;
+
+
+        //Berekening
+        double BMI = Convert.ToDouble(gewicht) / (lengteM * lengteM);
+
+        //resultaat
+        Console.Write($"Je BMI bedraagt: " + (BMI = Math.Round(BMI, 2)));
+    }
+}
---
+++ github template(1)/SlnLes02VariabelenBerekeningen/WpfBmi/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBMI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string lengteCm = txtBxLengte.Text;
+            double lengteM = Convert.ToDouble(lengteCm) / 100;
+            string gewicht = txtBxGewicht.Text;
+            double BMI = Convert.ToDouble(gewicht) / (Convert.ToDouble(lengteM) * Convert.ToDouble(lengteM));
+
+            lblBMI.Content = $"BMI: " + (BMI = Math.Round(BMI, 2));
+        }
+    }
+}
