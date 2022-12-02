4554282c1e2df4f67fdf1e08cb42ca77bbb54a08
JawadChaieb <jawad.chaieb@student.odisee.be>
2022-12-02 16:32:16 +0100

    1

---
+++ github template(1)/SlnLes02VariabelenBerekeningen/Console.seconden/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // seconden invoegen
+        Console.Write("Geef het aantal seconden: ");
+        int aantalSeconden = Convert.ToInt32(Console.ReadLine());
+
+        //uren berekening
+        int uren = aantalSeconden / 3600;
+
+        int restUren = aantalSeconden % 3600;
+
+        //minuten berekening
+        int minuten = restUren / 60;
+
+        int restMinuten = restUren % 60;
+
+        //seconden berekening
+        int seconden = restMinuten / 1;
+
+        int dagen = aantalSeconden / (24 * 60 * 60);
+
+        //Het omzet van seconden naar uren minuten en seconden
+        Console.Write($"Dit zijn ");
+        Console.WriteLine(Convert.ToString(dagen).PadLeft(2, '0') +
+            " dagen, " +
+            Convert.ToString(uren).PadLeft(2, '0') +
+            " uren, " +
+            Convert.ToString(minuten).PadLeft(2, '0') +
+            " minuten " +
+            Convert.ToString(seconden).PadLeft(2, '0') +
+            " en seconden ");
+        Console.ReadKey();
+
+    }
+}
