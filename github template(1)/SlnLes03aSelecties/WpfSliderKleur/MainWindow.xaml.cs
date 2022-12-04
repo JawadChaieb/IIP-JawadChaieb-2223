00eb1eafe9af069e688c28705e516382d1d622f5
JawadChaieb <jawad.chaieb@student.odisee.be>
2022-12-04 16:45:22 +0100

    1

---
+++ github template(1)/SlnLes03aSelecties/WpfSliderKleur/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSliderKleur
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void sldKleur_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            ///value slider
+            txtblkWaarde.Text = Convert.ToString(sldKleur.Value);
+
+            ///Value kleiner en gelijk aan 25 dan groen
+            if (Math.Round(sldKleur.Value) <= 25)
+            {
+                ///kleur aan txtblkWaarde
+                txtblkWaarde.Foreground = Brushes.Green;
+            }
+            ///Value groter dan 25 of kleiner en gelijk aan 50 dan tekstkleur geel
+            else if ((Math.Round(sldKleur.Value) > 25) && (sldKleur.Value <= 50))
+            {
+                txtblkWaarde.Foreground = Brushes.Yellow;
+            }
+            ///Value groter is dan 50 of kleiner en gelijk aan 75 dan oranje
+            else if ((Math.Round(sldKleur.Value) > 50) && (sldKleur.Value <= 75))
+            {
+                txtblkWaarde.Foreground = Brushes.Orange;
+            }
+            ///niet een van de vorige is dan wordt het rood
+            else
+            {
+                txtblkWaarde.Foreground = Brushes.Red;
+            }
+        }
+    }
+}
