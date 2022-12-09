883fe809515695fc21e4973f293f6ea8b3f0fe5a
JawadChaieb <jawad.chaieb@student.odisee.be>
2022-12-09 21:27:51 +0100

    1

---
+++ github template(1)/SlnLes03aSelecties/WpfRaden/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRaden
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int raad;
+        int pogingenOver = 3;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Random rnd = new Random();
+            raad = rnd.Next(1, 10);
+
+            lblResultaat.Foreground = Brushes.BlueViolet;
+        }
+        private void btnControleer_Click(object sender, RoutedEventArgs e)
+        {
+
+            int gok = Convert.ToInt32(txtbxGok.Text);
+
+            pogingenOver--;
+
+            if (gok > raad)
+            {
+                lblResultaat.Content = ($"Te hoog ! Je beht nog {pogingenOver} pogingen over");
+            }
+
+            else if (gok < raad)
+            {
+                lblResultaat.Content = ($"Te laag ! Je beht nog {pogingenOver} pogingen over");
+            }
+
+            else
+            {
+                lblResultaat.Content = ("Je hebt gewonnen !!!");
+
+                btnControleer.IsEnabled = false;
+            }
+
+            if (gok == raad && pogingenOver == 0)
+            {
+                lblResultaat.Content = ("Je hebt gewonnen !!!");
+                btnControleer.IsEnabled = false;
+            }
+
+            else if (pogingenOver == 0)
+            {
+                lblResultaat.Content = ("Je hebt geen pogingen meer ! Herstart het programma om te spelen");
+
+                btnControleer.IsEnabled = false;
+            }
+        }
+    }
+}
